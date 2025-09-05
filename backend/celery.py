833acf07c503413a65f0f836c8c62867f88b865f
PrathamTagline <prathamp.tagline@gmail.com>
2025-09-05 11:37:30 +0530

    celery task now start immediately and then its schedule by celery beat

--- backend/celery.py
+++ backend/celery.py
@@ -10,3 +10,23 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks([
     "backend.services",
 ])
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    from backend.services.tasks import save_tree_data_task
+    from backend.services.tasks import save_market_ids_for_all_events
+    password = os.getenv("DECRYPTION_KEY")
+    # Run immediately once at startup
+    sender.send_task("backend.services.tasks.save_tree_data_task")
+    sender.send_task("backend.services.tasks.save_market_ids_for_all_events", args=[password])
+    # Then run every 10 minutes
+    sender.add_periodic_task(
+        60.0 * 45,  # seconds
+        save_tree_data_task.s(),
+        name="Save tree data every 10 min",
+    )
+    sender.add_periodic_task(
+        60.0 * 45,  # seconds
+        save_market_ids_for_all_events.s(password=password),
+        name="Save market IDs for all events every 2 min",
+    )
--- backend/settings.py
+++ backend/settings.py
@@ -125,15 +125,6 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Kolkata"
 
 CELERY_BEAT_SCHEDULE = {
-    "save-tree-data-every-10-min": {
-        "task": "backend.services.tasks.save_tree_data_task",
-        "schedule": 600.0,
-    },
-    "save-market-ids-every-10-min": {
-        "task": "backend.services.tasks.save_market_ids_for_all_events",
-        "schedule": 600.0,
-        "args": (os.getenv("DECRYPTION_KEY"),),
-    },
     "fetch-odds-every-second": {
         "task": "backend.services.tasks.fetch_odds_for_all_events",
         "schedule": 1.0,
